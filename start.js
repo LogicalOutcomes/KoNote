4b7aa3c99b587b1df6482dce40eaffd49d1501f9
Andrew Appleby <andrewappleby22@gmail.com>
2015-07-07 20:46:09 -0400

    Ensure nw dependency and installation match
    > Makes sure the package.json dependency aren’t out of synch before the
    app starts up. This is crucial for deployments/installations.
    
    > Ref issue #89
    
    Reviewer: @timmclean

--- start.js
+++ start.js
@@ -2,6 +2,26 @@
 	var Stylus = require('stylus');
 	var Fs = require('fs');
 
+	// Ensure package.json NW dependancy and installed NW are the same version
+	Fs.readFile('package.json', 'utf8', function (err, data) {
+		if (err) { throw err }
+		var pkg = JSON.parse(data)
+
+		var nwName;
+		switch(pkg.scripts.start) {
+			case "nodewebkit .": nwName = "nodewebkit"; break;
+			case "nw .": nwName = "nw"; break;
+			default: throw "Unknown nwjs name found in package.json for scripts.start"
+		}
+
+		var nwPackage = pkg.dependencies[nwName].replace(/[^0-9.]/g, "")
+		var nwRunning = process.versions['node-webkit']	
+		
+		if (nwPackage !== nwRunning) {
+			throw "Unmatched NW Versions: [package.json: "+ nwPackage + "] [installed: " + nwRunning + "]"
+		}
+	})
+
 	// In order to avoid the need for Grunt or a similar build system,
 	// we'll compile the Stylus code at runtime.
 	var mainStylusCode = Fs.readFileSync('main.styl', {encoding: 'utf-8'});
