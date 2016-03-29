caac98f3905aa9a98ef26b5ca1b721b173a11e78
conceptualspace <dev@conceptualspace.net>
2016-03-29 15:02:07 -0400

    ignore dev config

--- Gruntfile.js
+++ Gruntfile.js
@@ -47,7 +47,8 @@ module.exports = function(grunt) {
 					{
 						src: [
 							'package.json',
-							'src/**'
+							'src/**',
+							'!src/config/develop.json'
 						],
 						dest: 'build/releases/temp/<%= grunt.task.current.args[0] %>/',
 						filter: 'isFile',
