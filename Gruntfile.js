f5b6f3d4522192ae970e2780543e6911bcf1aa2b
conceptualspace <dev@conceptualspace.net>
2016-10-13 02:50:23 -0400

    update nw to 0.18

--- Gruntfile.js
+++ Gruntfile.js
@@ -216,11 +216,11 @@ module.exports = function(grunt) {
 			},
 			nwjswin: {
 				cwd: 'build/releases/temp/',
-				cmd: 'nwb nwbuild -v 0.16.1 -p win32 --win-ico ./<%= grunt.task.current.args[0] %>/src/icon.ico -o ./nwjs-<%= grunt.task.current.args[0] %>/ --side-by-side ./<%= grunt.task.current.args[0] %>/'
+				cmd: 'nwb nwbuild -v 0.18.0 -p win32 --win-ico ./<%= grunt.task.current.args[0] %>/src/icon.ico -o ./nwjs-<%= grunt.task.current.args[0] %>/ --side-by-side ./<%= grunt.task.current.args[0] %>/'
 			},
 			nwjsosx: {
 				cwd: 'build/releases/temp/',
-				cmd: 'nwb nwbuild -v 0.16.1 -p osx64 --mac-icns ./<%= grunt.task.current.args[0] %>/src/icon.icns -o ./nwjs-<%= grunt.task.current.args[0] %>/ --side-by-side ./<%= grunt.task.current.args[0] %>/'
+				cmd: 'nwb nwbuild -v 0.18.0 -p osx64 --mac-icns ./<%= grunt.task.current.args[0] %>/src/icon.icns -o ./nwjs-<%= grunt.task.current.args[0] %>/ --side-by-side ./<%= grunt.task.current.args[0] %>/'
 			}
 		},
 		appdmg: {
