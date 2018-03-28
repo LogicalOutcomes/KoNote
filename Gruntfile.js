5283381486129e8058d5859e5fd4d1928c431774
conceptualspace <dev@conceptualspace.net>
2018-03-27 20:17:43 -0400

    fix formatting

--- Gruntfile.js
+++ Gruntfile.js
@@ -93,7 +93,7 @@ module.exports = function(grunt) {
 				src: [
 					'**',
 					'!**/AUTHOR*',
-                    '!**/assets*',
+					'!**/assets*',
 					'!**/bower.json',
 					'!**/changelog*',
 					'!**/changes*',
